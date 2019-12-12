453df49f08e7dc2a7f3daa98d352a76dd7cd04cc
sun <xiaoxing440824@outlook.com>
2019-12-12 12:00:00 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './core/main-layout/main-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', loadChildren: './modules/main/main.module#MainModule'},
+      {path: 'dashboard', loadChildren: './modules/main/main.module#MainModule'}
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
